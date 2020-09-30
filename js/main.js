9d43966a8382de5819be4db565bf5a4aeb2f3b90
bekir erdem <l3ekirerdem@gmail.com>
2020-09-30 17:58:37 +0300

    main showcase düzeltildi

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $('.Header-switch').on('click', function () {
-   if ($('Header').is('.menu-show')) {
-       $('.Header').removeClass('menu-show')
-   } else {
-    $('.Header').addClass('menu-show')
-   }
-})
+    console.log("d")
+    if ($('Header').is('.menu-show')) {
+      $('.Header').removeClass('menu-show')
+    } else {
+      $('.Header').addClass('menu-show')
+    }
+  })
+
